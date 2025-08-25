bd14aefa6550fd923a4f254d8694a9ca3f8b33d0
Lclaxton06 <122897992+Lclaxton06@users.noreply.github.com>
2025-08-25 10:42:47 -0500

    LPC 8/25/25 Added Task 2 for Assignment 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,39 @@
 public class HelloWorld { 
-    String name; public HelloWorld(String name) { 
+    private final String name;
+    private final int age;
+
+    public HelloWorld(String name) { 
         this.name = name; 
-    } public void greet() { 
+        this.age = 19; // default value if age not provided
+    }
+
+    public HelloWorld(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void greet() { 
         System.out.println("Hello, " + name + "!"); 
-    } public static void main(String[] args) { 
-        HelloWorld student = new HelloWorld("Lucas"); student.greet(); 
+    }
+
+    // Overloaded greet method
+    public void greet(String customMessage) {
+        System.out.println(customMessage + ", " + name + "!");
+    }
+
+    public void introduce() {
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+    }
+
+    public static void main(String[] args) { 
+        HelloWorld student1 = new HelloWorld("Lucas");
+        student1.greet();
+        student1.greet("Welcome");
+        student1.introduce();
+
+        HelloWorld student2 = new HelloWorld("Alex", 21);
+        student2.greet();
+        student2.greet("Hi there");
+        student2.introduce();
     } 
 }
